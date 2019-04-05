0515d76c27a36bcc796ff42849d6ac4d2bcbf4eb
BradHick <bradleyhrm@gmail.com>
2019-04-05 17:59:56 -0300

    feat: create first model

---
+++ model/events.js
@@ -0,0 +1,66 @@
+import Immutable from 'seamless-immutable';
+
+const initialState = new Immutable({
+  events: [],
+  errors: {},
+  hasMoreEvents: true,
+  eventPage: 1,
+  loading: false
+});
+
+const event = {
+  state: initialState,
+  reducers: {
+    fetchEventsFulfiled: (state, payload) => {
+      if (payload.length > 0){
+        return state.merge({
+          events: state.commits.concat(payload),
+          eventPage: state.commitPage + 1,
+          loading: false
+        });
+      }
+      return state.merge({
+        loading: false,
+        hasMoreEvents: false
+      });
+    },
+    fetchEventsPending: (state) => {
+      return state.merge({
+        loading: true
+      });
+    },
+    resetEvents: (state) => {
+      return state.merge({
+        events: [],
+        hasMoreEvents: true,
+        eventPage: 1,
+        errors: {},
+        loading: false
+      });
+    },
+    fetchEventsRejected: (state, payload) => {
+      return state.merge({
+        errors: payload.errors || payload,
+        loading: false
+      });
+    }
+  },
+  effects: (dispatch) => ({
+    fetchCommits(params,state){
+      const { eventPage, hasMoreEvents } = state.event;
+      if (hasMoreEvents){
+        dispatch.commit.fetchCommitsPending();
+        //https://frontend-test.agendaedu.com/api/events?limit=1;page=1
+        return fetch(`https://frontend-test.agendaedu.com/api/events?limit=10;page=${eventPage}`)
+          .then(res => {
+            dispatch.event.fetchEventsFulfiled(res.data);
+          })
+          .catch(err =>{
+            dispatch.event.fetchEventsRejected(err);
+          });
+      }
+    }
+  })
+};
+
+export default event;
