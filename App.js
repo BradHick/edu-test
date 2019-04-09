b8db92e3cb3a6da9f4ccf0b0068072b983afbb5f
BradHick <bradleyhrm@gmail.com>
2019-04-09 10:30:08 -0300

    refactor(app): use persist plugin

--- App.js
+++ App.js
@@ -3,17 +3,23 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 import store from './store';
 import { Provider } from 'react-redux';
 import Router from './router';
+import { getPersistor } from '@rematch/persist'
+import { PersistGate } from 'redux-persist/lib/integration/react-native'
+
+const persistor = getPersistor()
 
 
 export default class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <View style={styles.container}>
-          <Text>Testes</Text>
-          <Router />
-        </View>
-      </Provider>
+      <PersistGate persistor={persistor}>
+        <Provider store={store}>
+          <View style={styles.container}>
+            <Text>Testes</Text>
+            <Router />
+          </View>
+        </Provider>
+      </PersistGate>
     );
   }
 }
--- store/index.js
+++ store/index.js
@@ -1,8 +1,16 @@
 import { init } from '@rematch/core';
 import models from '../model';
+import createRematchPersist from '@rematch/persist'
+
+const persistPlugin = createRematchPersist({
+  whitelist: ['auth'],
+  throttle: 5000,
+  version: 1,
+})
 
 const store = init( {
-  models
+  models,
+  plugins: [persistPlugin]
 } );
 window.store = store;
 
