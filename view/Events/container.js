530c58ac0a0f7f9c430ac77a4978ce70e99c08de
BradHick <bradleyhrm@gmail.com>
2019-04-10 17:47:59 -0300

    feat(Events): prepare container

--- view/Events/container.js
+++ view/Events/container.js
@@ -1,9 +1,14 @@
 import { connect } from 'react-redux';
 
 const mapStateToProps = (state) => ({
+  events: state.event.events,
+  errors: state.event.errors,
+  loading: state.event.loading,
+  hasMoreEvents: state.event.hasMoreEvents
 });
 
 const mapDispatchToProps = (dispatch) => ({
+  fetchEvents: dispatch.event.fetchEvents
 });
 
 export default component => connect(mapStateToProps,mapDispatchToProps)(component);
